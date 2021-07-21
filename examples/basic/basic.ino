11b55426500ee9c306c0c58234aac1e8c9c503ff
Saurabh Datta <hi@dattasaurabh.com>
2021-07-21 19:11:51 +0800

    example refined

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -18,60 +18,68 @@ const unsigned long secPeriod = 1000; //the value is a number of milliseconds
 
 void setup()
 {
-    Serial.begin(115200);
-    while (!Serial)
+  Serial.begin(115200);
+  while (!Serial)
+  {
+  }
+
+  while (rtc.begin() == false)
+  { // un-shifted default address for RV-8803 is 0x32. Check library
+    rtcAvailable = false;
+    Serial.println(F("Something went wrong, ctheck wiring"));
+    delay(2000);
+  }
+  rtcAvailable = true;
+
+  // ---- Set the time for the RTC as the time during code compilation ---- //
+  Serial.println(F("Setting to compiler time..."));
+  if (rtc.setToCompilerTime() == false)
+  {
+    Serial.println("Something went wrong setting the time");
+    while (true)
     {
+      delay(2000);
     }
+  }
+  Serial.println(F("Compiler Time set!"));
 
-    while (rtc.begin() == false)
-    { // un-shifted default address for RV-8803 is 0x32. Check library
-        rtcAvailable = false;
-        Serial.println(F("Something went wrong, ctheck wiring"));
-        delay(2000);
-    }
-    rtcAvailable = true;
-
-    // ---- Set the time for the RTC as the time during code compilation ---- //
-    Serial.println(F("Setting to compiler time..."));
-    if (rtc.setToCompilerTime() == false)
-    {
-        Serial.println("Something went wrong setting the time");
-        while (true)
-        {
-            delay(2000);
-        }
-    }
-    Serial.println(F("Compiler Time set!"));
-
-    // ---- OR custom time setting method ---- //
-    //  Serial.println(F("Setting custom time..."));
-    //  if (rtc.setTime(sec, minute, hour, weekday, date, month, year) == false) {
-    //    Serial.println("Something went wrong setting the time");
-    //    while (true) {
-    //      delay(2000);
-    //    }
-    //  }
-    //  Serial.println(F("Custom Time set!"));
+  // ---- OR custom time setting method ---- //
+  //  Serial.println(F("Setting custom time..."));
+  //  if (rtc.setTime(sec, minute, hour, weekday, date, month, year) == false) {
+  //    Serial.println("Something went wrong setting the time");
+  //    while (true) {
+  //      delay(2000);
+  //    }
+  //  }
+  //  Serial.println(F("Custom Time set!"));
 }
 
 void loop()
 {
-    if (rtcAvailable)
+  if (rtcAvailable)
+  {
+    // updateTime i.e read registers, ** must for getting current time
+    if (rtc.updateTime())
     {
-        // updateTime i.e read registers, ** must for getting current time
-        if (rtc.updateTime())
-        {
-            Serial.println(rtc.getSeconds());
-            /*
-        .getMinutes();
-        .getHours();
-        .getDate();
-        .getWeekday();
-        .getMonth();
-        .getYear();
+      Serial.print(rtc.getDate());
+      Serial.print("/");
+      Serial.print(rtc.getMonth());
+      Serial.print("/");
+      Serial.println(rtc.getYear());
+
+      Serial.print("WEEKDAY: ");
+      Serial.println(rtc.getWeekday());
+
+      Serial.print(rtc.getHours());
+      Serial.print(":");
+      Serial.print(rtc.getMinutes());
+      Serial.print(":");
+      Serial.println(rtc.getSeconds());
+      Serial.println("");
+      /*
         .updateTimeArray(); // To push time read from registers to an int array
       */
-        }
     }
-    delay(1000);
+  }
+  delay(1000);
 }
